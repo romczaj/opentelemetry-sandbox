5af92796814ff3716e63f6670a327569cb12b9f0
Piotr Romaniuk <ajemromek@gmail.com>
2024-08-27 22:37:11 +0200

    init

---
+++ order-service/src/main/java/pl/romczaj/opentelemetry/order/OrderApplication.java
@@ -0,0 +1,11 @@
+package pl.romczaj.opentelemetry.order;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
